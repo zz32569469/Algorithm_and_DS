b85b0467bd35c33fd54f3bf6da9addece9494d95
zz32569469 <100284518+zz32569469@users.noreply.github.com>
2022-08-03 20:39:24 +0800

    Create pbds.cpp

---
+++ pbds.cpp
@@ -0,0 +1,7 @@
+//pbds
+
+#include<bits/extc++.h>
+
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
+				pbds;//rb_tree//find_by_order(k);
