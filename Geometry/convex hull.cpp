f47c0967e31c313a67037351ea9db1a8bd81434e
zz32569469 <100284518+zz32569469@users.noreply.github.com>
2022-10-12 19:16:35 +0800

    Update convex hull.cpp

--- Geometry/convex hull.cpp
+++ Geometry/convex hull.cpp
@@ -1,6 +1,3 @@
-vector<pt> a;
-vector<pt> stk;
-
 struct pt{
     ll x,y;
     pt(){}
@@ -35,27 +32,30 @@ struct pt{
     }
 };
 
-void convex hull{
-       int top=0;
-       sort(a.begin(),a.end());
-       stk.clear();
+vector<pt> a;
+vector<pt> stk;
 
-       for(int i=0;i<n;i++){
-           while(top>=2&&cross(stk[top-2],stk[top-1],a[i])<0){
-               stk.pop_back();
-               top--;
-           }
-           stk.push_back(a[i]);
-           top++;
-       }
+void convex_hull(){
+    int top=0;
+    sort(a.begin(),a.end());
+    stk.clear();
 
-       for(int i=n-2,tt=top+1;i>=0;i--){
-           while(top>=tt&&cross(stk[top-2],stk[top-1],a[i])<0){
-               stk.pop_back();
-               top--;
-           }
-           stk.push_back(a[i]);
-           top++;
-       }
-       stk.pop_back();
+    for(int i=0;i<n;i++){
+        while(top>=2&&cross(stk[top-2],stk[top-1],a[i])<0){
+            stk.pop_back();
+            top--;
+        }
+        stk.push_back(a[i]);
+        top++;
+    }
+
+    for(int i=n-2,tt=top+1;i>=0;i--){
+         while(top>=tt&&cross(stk[top-2],stk[top-1],a[i])<0){
+            stk.pop_back();
+            top--;
+        }
+        stk.push_back(a[i]);
+        top++;
+    }
+    stk.pop_back();
 }
