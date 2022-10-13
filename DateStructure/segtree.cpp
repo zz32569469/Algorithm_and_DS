043bc8fe6bdc2524bc9ef5c52805f51a3c560c70
zz32569469 <100284518+zz32569469@users.noreply.github.com>
2022-10-13 18:42:48 +0800

    Update segtree.cpp

--- DateStructure/segtree.cpp
+++ DateStructure/segtree.cpp
@@ -1,12 +1,31 @@
 #define cl(x) x*2
 #define cr(x) x*2+1//1-base
 
-//假設維護區間最小值
+//假設維護區間和
 
 int segtree[MXN<<2];
 int a[MXN];
+int tag[MXN<<2];
 
- 
+void push(int i,int l,int r){
+    if(tag[i] != 0){
+        segtree[i] += tag[i]*(r-l+1); 
+        if(l != r){ 
+            tag[cl(i)] += tag[i];
+            tag[cr(i)] += tag[i];
+        }
+        tag[i] = 0;
+    }
+}
+
+void pull(int i,int l,int r){
+    int mid = (l+r)/2;
+    if(l==r) return ;
+    push(cl(i),l,mid); push(cr(i),mid+1,r);
+    segtree[i] = segtree[cl(i)] + segtree[cr(i)];
+}
+
+
 void build(int i,int l,int r){//初始化，不一定要有
     if(l == r){ 
         segtree[i] = a[l];
@@ -16,17 +35,19 @@ void build(int i,int l,int r){//初始化，不一定要有
     build(cl(i),l,mid); 
     build(cr(i),mid+1,r);
   
-    segtree[i] = min(segtree[cl(i)], segtree[cr(i)]);
+    segtree[i] = segtree[cl(i)]+segtree[cr(i)];
 }
  
 int query(int i,int l,int r,int ql,int qr){
+    push(i,l,r);
+    
     if(ql <= l && r <= qr){ 
         return segtree[i];
     }
-    int mid=(l+r)/2, ret=inf;
- 
-    if(ql<=mid) ret = min(ret, query(cl(i),l,mid,ql,qr));
-    if(qr> mid) ret = min(ret, query(cr(i),mid+1,r,ql,qr));
+    int mid=(l+r)/2, ret=0;
+    if(ql<=mid) ret+=query(cl(i),l,mid,ql,qr);
+    if(qr> mid) ret+=query(cr(i),mid+1,r,ql,qr);
+    pull(i,l,r);
     return ret;
 }
  
@@ -40,4 +61,16 @@ void update(int i,int l,int r,int pos,int val){//單點修改
     else update(cr(i),mid+1,r,pos,val);
  
     segtree[i] = min(segtree[cl(i)], segtree[cr(i)]);
+}
+
+void update2(int i,int l,int r,int ql,int qr,int v,int op){//區間修改
+    push(i,l,r);
+    if(ql<=l && r<=qr){
+        tag[i] += v;
+        return;
+    }
+    int mid=(l+r)/2;
+    if(ql<=mid)  update2(cl(i),l,mid,ql,qr,v,op);
+    if(qr> mid)  update2(cr(i),mid+1,r,ql,qr,v,op);
+    pull(i,l,r);
 }
