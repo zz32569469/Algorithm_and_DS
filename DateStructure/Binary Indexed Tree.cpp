ebe69d4890e799bc375fc90cd0c4e478c6d56bd1
zz32569469 <100284518+zz32569469@users.noreply.github.com>
2022-10-13 21:10:47 +0800

    Create Binary Indexed Tree.cpp

---
+++ DateStructure/Binary Indexed Tree.cpp
@@ -0,0 +1,19 @@
+#define lowbit(x) x&(-x)
+
+int bit[MXN];
+
+int query(int x){
+    int ret=0;
+    while(x){
+        ret += bit[x];
+        x -= lowbit(x);
+    }
+    return ret;
+}
+
+void update(int x,int v){
+    while(x<=n){
+        bit[x] += v;
+        x += lowbit(x);
+    }
+}
