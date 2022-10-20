c3bd6e20d13e87634b86f3472555cdede9bb6fde
zz32569469 <100284518+zz32569469@users.noreply.github.com>
2022-10-21 06:16:35 +0800

    Update Undirected-Eulur_circuit.cpp

--- graph/Undirected-Eulur_circuit.cpp
+++ graph/Undirected-Eulur_circuit.cpp
@@ -15,7 +15,7 @@ void dfs(int x){
 int Eulur_circuit(int s,int n,int m){// s 為起點， n 為點數， m 為邊數
     for(int i=1;i<=n;i++) if(deg[i]&1) return 0;//條件每個點度數為偶數
     
-    dfs(s,s);
+    dfs(s);
     reverse(path.begin(),path.end());
     if(path.size()!=m+1) return 0; 
     return 1;
