1e00d6e3116352ccc0b7d9750d3cbe940d87de16
zz32569469 <100284518+zz32569469@users.noreply.github.com>
2023-03-10 00:41:32 +0800

    Update factorial&choose.cpp

--- math/factorial&choose.cpp
+++ math/factorial&choose.cpp
@@ -1,19 +1,19 @@
 const ll MOD=1e9+7;
 const ll MXN=5e3+5;
 
-ll fac[MXN],inv[MXN];
-void init(){
+ll fac[MXN],inv[MXN],invf[MXN];
+void initf(){
     fac[1]=fac[0]=1;
 	inv[0]=inv[1]=1;
-    for(int i=1;i<MXN;i++){
+    invf[0]=invf[1]=1;
+    for(int i=2;i<MXN;i++){
         fac[i]=(fac[i-1]*i)%MOD;
         inv[i]=MOD-(MOD/i*inv[MOD%i])%MOD;
+        invf[i]=invf[i-1]*inv[i]%MOD;
     }
-
-    for(int i=1;i<MXN;i++) inv[i]=(inv[i]*inv[i-1])%MOD;
 }
 
 ll C(int n,int m){
     if(n<m||n<0) return 0;
-    return (((fac[n]*inv[m])%MOD)*inv[n-m])%MOD;
+    return (((fac[n]*invf[m])%MOD)*invf[n-m])%MOD;
 }
