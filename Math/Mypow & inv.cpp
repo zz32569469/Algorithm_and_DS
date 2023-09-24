ee7a58a7a8a42da7967cd3bf205c0cb1b7a04b92
n4make <100284518+zz32569469@users.noreply.github.com>
2023-09-24 16:42:48 +0800

    Create Mypow & inv.cpp

---
+++ Math/Mypow & inv.cpp
@@ -0,0 +1,16 @@
+int mypow(int x, int y ,int mod = MOD) {
+    x %= mod;
+    int res = 1 % mod;
+    while(y){
+        if(y & 1) {
+            res = res * x % mod;
+        }
+        y >>= 1;
+        x = x * x % mod;
+    }
+    return res;
+}
+
+int get_inv(int x) {
+    return mypow(x , MOD - 2);
+}
