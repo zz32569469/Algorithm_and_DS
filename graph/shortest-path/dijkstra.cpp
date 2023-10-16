8db35092dc92f17d25c7b53400171240cd760936
n4make <100284518+zz32569469@users.noreply.github.com>
2023-10-16 17:52:57 +0800

    Update dijkstra.cpp

--- graph/shortest-path/dijkstra.cpp
+++ graph/shortest-path/dijkstra.cpp
@@ -1,35 +1,48 @@
-struct rec{
-    int to,w;
-};
-int dis[MXN];//原點到其他點的最短距離
-bool vst[MXN];
-vector<rec>edge[MXN];
+struct dijkstra{
+    static const int MXN = 2e5+5;
+    static const int INF  = 0x3f3f3f3f3f3f3f3f;
 
-bool relaxtion(int u,int v,int w){
-    if(dis[v]>dis[u]+w){
-        dis[v]=dis[u]+w;
-        return 1;
+    struct info{
+        int to, w;
+    };
+
+    int dis[MXN];//原點到其他點的最短距離
+    bool vst[MXN];
+    vector<info>edge[MXN];
+
+    void add_edge(int u, int v, int w){ //加入邊
+        edge[u].PB({v, w});
     }
-    return 0;
-}
 
-void dijkstra(int source){//原點
-	memset(dis,INF,sizeof(dis));
-    dis[source]=0;
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-
-    pq.push({0,source});
-
-    while(!pq.empty()){
-        auto [d,idx]=pq.top();
-        pq.pop();
-        if(vst[idx]) continue;
-
-        vst[idx]=1;
-        for(int i=0;i<edge[idx].size();i++){
-            int u=idx,v=edge[idx][i].to,w=edge[idx][i].w;
-
-            if(relaxtion(u,v,w)) pq.push({dis[v],v});
+    bool relaxtion(int u,int v,int w){
+        if(dis[v]>dis[u]+w){
+            dis[v]=dis[u]+w;
+            return 1;
         }
+        return 0;
     }
-}//dijkstra 無負邊權 O(NlogN+M)
+
+    int solve(int src, int dst){//原點
+        memset(dis,INF,sizeof(dis));
+        dis[src]=0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+
+        pq.push({0,src});
+
+        while(!pq.empty()){
+            auto [d, idx]=pq.top();
+            pq.pop();
+            if(vst[idx]) continue;
+
+            vst[idx]=1;
+            for(int i=0;i<edge[idx].size();i++){
+                int u=idx,v=edge[idx][i].to,w=edge[idx][i].w;
+
+                if(relaxtion(u,v,w)) pq.push({dis[v],v});
+            }
+        }
+
+        return dis[dst];
+    }//dijkstra 無負邊權 O(NlogN+M)
+
+};
