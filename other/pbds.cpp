80eb528eaf72584ec8077cfcf1e35c1f07705bab
n4make <100284518+zz32569469@users.noreply.github.com>
2023-10-16 18:38:10 +0800

    Update pbds.cpp

--- other/pbds.cpp
+++ other/pbds.cpp
@@ -1,26 +1,20 @@
 #include<bits/extc++.h>
 
 using namespace __gnu_pbds;
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
-				t; 
+tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> t; 
 /*
-定义一颗红黑树
-int 关键字类型
-null_type无映射(低版本g++为null_mapped_type)
-less<int>从小到大排序
-rb_tree_tag 红黑树（splay_tree_tag）
-tree_order_statistics_node_update结点更新
+定義一顆紅黑樹
+int 關鍵字類型
+null_type無映射
+less<int> 從小到大排序
+rb_tree_tag 紅黑樹（splay_tree_tag）
+tree_order_statistics_node_update結點更新
 插入t.insert();
-删除t.erase();
-求k在树中是第几大:t.order_of_key();
-求树中的第k大:t.find_by_order();
-前驱:t.lower_bound();
-后继t.upper_bound();
-a.join(b)b并入a 前提是两棵树的key的取值范围不相交
-a.split(v,b)key小于等于v的元素属于a，其余的属于b
-T.lower_bound(x)   >=x的min的迭代器
-T.upper_bound((x)  >x的min的迭代器
-T.find_by_order(k) 有k个数比它小的数
+刪除t.erase();
+求k在樹中是第幾大: t.order_of_key();
+求樹中的第k大: t.find_by_order();
+t.lower_bound();
+t.upper_bound();
+a.join(b) b 併入 a 前提是兩棵樹的 key 的取值範圍不相交
+a.split(v,b) key 小於等於v的元素屬於 a，其餘的屬於 b
 */
-
-pbds s;//s.find_by_order(k);s.order_by_key(x);
