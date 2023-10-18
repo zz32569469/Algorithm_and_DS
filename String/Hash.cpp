0d7b80f0a165210fe6bd7d462c32d179b2fd43da
n4make <100284518+zz32569469@users.noreply.github.com>
2023-10-18 18:48:54 +0800

    Update Hash.cpp

--- String/Hash.cpp
+++ String/Hash.cpp
@@ -2,7 +2,6 @@ const int P=37;
 //71,57,31
 
 int ha[MXN],a[MXN],base[MXN];
-map<int,int>mp;
 
 int segment_Hash(int l,int r){
     if(l==0) return ha[r];
@@ -22,12 +21,5 @@ void creatHash(string s){
     ha[0]=a[0];
 
     for(int i=1;i<s.size();i++) ha[i]=(P*ha[i-1]+a[i])%MOD;
-}
-
-int segment_Hash(int l,int r){
-    if(l==0) return ha[r];
-    int ret=(ha[r]-ha[l-1]*base[r-l+1])%MOD;
-    while(ret<0) ret=(ret+MOD)%MOD;
-    return ret;
 }
 
