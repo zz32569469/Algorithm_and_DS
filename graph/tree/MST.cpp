bf8640a346bd6966e0fdad1f10141758a2b90354
n4make <100284518+zz32569469@users.noreply.github.com>
2023-11-11 23:29:25 +0800

    Update MST.cpp

--- graph/tree/MST.cpp
+++ graph/tree/MST.cpp
@@ -1,41 +1,41 @@
-struct rec{
-    int u,v,w;
-    bool operator<(const rec _x){
-        return w<_x.w;
-    }
-};
+// 需要 disjoint_set (ds)
 
-vector<rec>edge;
-int f[MXN];
-
-void init(int n){
-    for(int i=1;i<=n;i++) f[i]=i;
-}
-
-int find(int x){
-    if(x==f[x]) return f[x];
-    return f[x]=find(f[x]);
-}
-
-void merge(int x,int y){
-    y=find(y),x=find(x);
-    if(y!=x) f[y]=x;
-}
-
-int MST(int n){
-    init(n);
-
-    sort(edge.begin(),edge.end());//權重排序
-
-    int sum=0,cnt=0;
-    for(int i=0;i<m;i++){
-        if(find(edge[i].u)!=find(edge[i].v)){
-            merge(edge[i].u,edge[i].v);
-            sum+=edge[i].w;
-            cnt++;
+struct MST{
+    int n, m;// 點，邊
+    struct rec{
+        int u, v, w;
+        bool operator<(const rec _x){
+            return w < _x.w;
         }
+    };
+    
+    vector<rec>edge;
+    disjoint_set ds;
+
+    void init(int _n, int _m){
+        n=_n, m=_m;
+        ds.init(n+1);
     }
+
+    void add_edge(int u, int v, int w){ //u -> v 權重為 w 
+        edge.PB({u, v, w});
+    }
+
+
+    int solve(){
+        sort(edge.begin(),edge.end());//權重排序
+
+        int sum=0, cnt=0;
+        for(int i=0;i<m;i++){
+            if(ds.FIND(edge[i].u)!=ds.FIND(edge[i].v)){
+                ds.MERGE(edge[i].u,edge[i].v);
+                sum+=edge[i].w;
+                cnt++;
+            }
+        }   
   
-    if(cnt!=n-1) return -1;//邊數量 < n-1 則無法形成 Minimum Spanning Tree
-    else return sum; // 回傳 Minimum Spanning Tree 的總權重
-}
+        if(cnt!=n-1) return -1;//邊數量 < n-1 則無法形成 Minimum Spanning Tree
+        else return sum; // 回傳 Minimum Spanning Tree 的總權重
+    }
+
+};//kruskal's algorithm
