8e52e8d67c3f610cddc4cdd01e5a18cff2102830
n4make <zz32569469@gmail.com>
2023-11-15 05:02:07 +0800

    new file:   other/init.cpp

---
+++ other/init.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define ll long long 
+#define ld long double 
+#define int long long
+#define endl '\n'; 
+#define cl(x) x*2 
+#define cr(x) x*2+1//1-base 
+#define lowbit(x) x&(-x) 
+#define PB push_back
+#define SZ(x) (int)x.size() 
+#define FZ(x) memset(x,  0,  sizeof(x)) 
+#define all(x) x.begin(), x.end()
+  
+using namespace std;
+ 
+ 
+//assert(false);  
+const int MXN = 1e6+5; 
+const int INF = 0x3f3f3f3f3f3f3f3f; 
+const int MOD = 998244353; 
+const int LgN = log2(1e6)+1; 
+const int P = 37; 
+const ld EXP = 1e-11;
+ 
+void solve(){
+    
+    
+}
+ 
+signed main(){ 
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
+ 
+    int t = 1; 
+    //cin >> t;
+ 
+    while(t--){
+        solve(); 
+        cout<<'\n'; 
+    } 
+}
