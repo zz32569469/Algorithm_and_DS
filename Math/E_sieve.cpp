f2079a3705a57d366190c1488e529a41310863f3
n4make <zz32569469@gmail.com>
2023-12-12 19:23:03 +0800

    modified:   Math/E_sieve.cpp

--- Math/E_sieve.cpp
+++ Math/E_sieve.cpp
@@ -2,14 +2,20 @@ struct E_sieve{
     vector<int>prime;
     vector<int>isPrime;
 
+    vector<vector<int>>dec;
+
     void init(int n){
         isPrime.resize(n+1);
+        dec.resize(n+1);
         isPrime[1]=isPrime[0]=1;
         
         for(int i=2;i<=n;i++){
             if(!isPrime[i]){
                 prime.PB(i);
-                for(int j=i;j*i<=n;j++) isPrime[i*j]=1;
+                for(int j=1;j*i<=n;j++){
+                    dec[i*j].PB(i);
+                    isPrime[i*j]=1;
+                }
             }
         }
     }
